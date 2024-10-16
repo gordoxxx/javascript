d8e811671b93b3628e523552811dceab93fbce20
gordoxxx <165900261+gordoxxx@users.noreply.github.com>
2024-10-16 08:26:53 -0300

    eu

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function mudaCor(){
-    const titulo = document.getElementById('titulo')   
+    const titulos = document.getElementById('titulo')   
     titulo.style.color = titulo.style.color === 'red' ? 'blue' : 'red'                                 
 }
